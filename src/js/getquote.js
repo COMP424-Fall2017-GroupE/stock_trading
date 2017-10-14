66825e616e763a3a85c02b4babaaee95df0939f3
andreifedorovforte <andrei.fedorov@fortegrp.net>
2017-10-14 17:31:59 -0500

    updated dashboard with a dynamic chart widget

--- src/js/getquote.js
+++ src/js/getquote.js
@@ -27,21 +27,16 @@ function getQuotes() {
             // render quote
             $(".render-quote").empty().append($quote);
 
-            // render chart
-            // $(".chart").append("<div>", new TradingView.widget({
-            //         "width": 500,
-            //         "height": 300,
-            //         "symbol": ticker,
-            //         "interval": "D",
-            //         "timezone": "Etc/UTC",
-            //         "theme": "Light",
-            //         "style": "1",
-            //         "locale": "en",
-            //         "toolbar_bg": "#f1f3f6",
-            //         "enable_publishing": false,
-            //         "allow_symbol_change": true,
-            //         "hideideas": true
-            //     }));
+            // update chart source
+            let chart_div = document.getElementsByTagName("div");
+            let new_chart =
+                `<iframe id="tradingview_7a914"
+                            src="https://s.tradingview.com/widgetembed/?symbol=${ticker}&amp;interval=D&amp;symboledit=1&amp;saveimage=1&amp;toolbarbg=f1f3f6&amp;studies=%5B%5D&amp;hideideas=1&amp;theme=Light&amp;style=1&amp;timezone=Etc%2FUTC&amp;studies_overrides=%7B%7D&amp;overrides=%7B%7D&amp;enabled_features=%5B%5D&amp;disabled_features=%5B%5D&amp;locale=en&amp;utm_source=localhost&amp;utm_medium=widget&amp;utm_campaign=chart&amp;utm_term=${ticker}"
+                            width="500" height="300" frameborder="0" allowtransparency="true" scrolling="no"
+                            allowfullscreen="">
+                </iframe>`;
+
+            chart_div[1].innerHTML = new_chart;
 
         } else {
             alert("Please enter ticker symbol");
