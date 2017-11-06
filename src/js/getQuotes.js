feb4a9f9b757982c701df2f9bae65093be433543
andreifedorovforte <andrei.fedorov@fortegrp.net>
2017-11-05 23:08:03 -0600

    simplified updating chart source

--- src/js/getQuotes.js
+++ src/js/getQuotes.js
@@ -40,15 +40,8 @@ function getQuotes() {
             $(".render-quote").empty().append($quote);
 
             // update chart source
-            let chart_div = document.getElementsByTagName("div");
-            let new_chart =
-                `<iframe id="tradingview_7a914"
-                            src="https://s.tradingview.com/widgetembed/?symbol=${ticker}&amp;interval=D&amp;symboledit=1&amp;saveimage=1&amp;toolbarbg=f1f3f6&amp;studies=%5B%5D&amp;hideideas=1&amp;theme=Light&amp;style=1&amp;timezone=Etc%2FUTC&amp;studies_overrides=%7B%7D&amp;overrides=%7B%7D&amp;enabled_features=%5B%5D&amp;disabled_features=%5B%5D&amp;locale=en&amp;utm_source=localhost&amp;utm_medium=widget&amp;utm_campaign=chart&amp;utm_term=${ticker}"
-                            width="500" height="300" frameborder="0" allowtransparency="true" scrolling="no"
-                            allowfullscreen="">
-                </iframe>`;
-
-            chart_div[1].innerHTML = new_chart;
+            let src = `https://s.tradingview.com/widgetembed/?symbol=${ticker}&amp;interval=D&amp;symboledit=1&amp;saveimage=1&amp;toolbarbg=f1f3f6&amp;studies=%5B%5D&amp;hideideas=1&amp;theme=Light&amp;style=1&amp;timezone=Etc%2FUTC&amp;studies_overrides=%7B%7D&amp;overrides=%7B%7D&amp;enabled_features=%5B%5D&amp;disabled_features=%5B%5D&amp;locale=en&amp;utm_source=localhost&amp;utm_medium=widget&amp;utm_campaign=chart&amp;utm_term=${ticker}`;
+            $("iframe").attr("src", src);
 
         } else {
             alert("Please enter ticker symbol");
