b40694c5f95a686a2ab7fb17b5cbc633c10c2d72
andreifedorovforte <andrei.fedorov@fortegrp.net>
2017-11-13 20:30:18 -0600

    updated node.js server

--- nodetest/server.js
+++ nodetest/server.js
@@ -2,21 +2,31 @@ var express = require('express');
 var http = require('http');
 var mongoose = require('mongoose');
 var app = express();
-var path = require('path');
-
-// mongodb connection uri
-var uri = "mongodb://afedorov:jwlupKsfzMXX6XNt@stocktrading-shard-00-00-reaq6.mongodb.net:27017,stocktrading-shard-00-01-reaq6.mongodb.net:27017,stocktrading-shard-00-02-reaq6.mongodb.net:27017/test?ssl=true&replicaSet=StockTrading-shard-0&authSource=admin";
 
 // start the server
+app.use(express.static(__dirname + "/../src/"));
 http.createServer(app).listen(3030);
 
 // establish database connection
-var promise = mongoose.createConnection(uri, {
-    useMongoClient: true
-    /* other options */
-});
-promise.on('open', function () {
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname + "/../src/dashboard.html"));
-    });
-});
+
+// var uri = "mongodb://afedorov:jwlupKsfzMXX6XNt@stocktrading-shard-00-00-reaq6.mongodb.net:27017,stocktrading-shard-00-01-reaq6.mongodb.net:27017,stocktrading-shard-00-02-reaq6.mongodb.net:27017/test?ssl=true&replicaSet=StockTrading-shard-0&authSource=admin";
+// var promise = mongoose.createConnection(uri, {
+//     useMongoClient: true
+//     /* other options */
+// });
+// promise.on('open', function (db) {
+//     /* do smth useful */
+//
+// });
+
+
+// app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname + "/../src/dashboard.html"));
+//     io.on('connection', function (socket) {
+//         socket.on('connexion_client', function(data) {
+//             res.redirect(path.join(__dirname + "/../src/dashboard.html"));
+//         });
+//     });
+// });
+
+
