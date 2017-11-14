9a260be381c509999a3e6559bcd386ad4840e3d8
andreifedorovforte <andrei.fedorov@fortegrp.net>
2017-11-13 19:23:10 -0600

    updated node.js server

--- nodetest/server.js
+++ nodetest/server.js
@@ -1,43 +1,22 @@
 var express = require('express');
 var http = require('http');
+var mongoose = require('mongoose');
 var app = express();
+var path = require('path');
+
+// mongodb connection uri
+var uri = "mongodb://afedorov:jwlupKsfzMXX6XNt@stocktrading-shard-00-00-reaq6.mongodb.net:27017,stocktrading-shard-00-01-reaq6.mongodb.net:27017,stocktrading-shard-00-02-reaq6.mongodb.net:27017/test?ssl=true&replicaSet=StockTrading-shard-0&authSource=admin";
+
+// start the server
 http.createServer(app).listen(3030);
 
-let uri = "mongodb://afedorov:jwlupKsfzMXX6XNt@stocktrading-shard-00-00-reaq6.mongodb.net:27017,stocktrading-shard-00-01-reaq6.mongodb.net:27017,stocktrading-shard-00-02-reaq6.mongodb.net:27017/test?ssl=true&replicaSet=StockTrading-shard-0&authSource=admin";
-
-let mongoose = require('mongoose');
-
+// establish database connection
 var promise = mongoose.createConnection(uri, {
     useMongoClient: true
     /* other options */
 });
-
 promise.on('open', function () {
     app.get("/", function (req, res) {
-        res.send("connected");
+        res.sendFile(path.join(__dirname + "/../src/dashboard.html"));
     });
 });
-
-// mongodb connection
-function databaseConn() {
-    var MongoClient = require('mongodb');
-    let uri = "mongodb://afedorov:jwlupKsfzMXX6XNt@stocktrading-shard-00-00-reaq6.mongodb.net:27017,stocktrading-shard-00-01-reaq6.mongodb.net:27017,stocktrading-shard-00-02-reaq6.mongodb.net:27017/test?ssl=true&replicaSet=StockTrading-shard-0&authSource=admin";
-
-    let mongoose = require('mongoose');
-    var promise = mongoose.createConnection(uri, {
-        useMongoClient: true
-        /* other options */
-    });
-
-    promise.on('open', function () {
-        return true;
-    });
-
-    // MongoClient.connect(uri, function(err, db) {
-    //     db.close();
-    //     return false;
-    // }).done(function () {
-    //     return true;
-    // });
-
-}
